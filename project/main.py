2c4fcaad294b390074e6d4ff95627b5cf2c67955
halbritt <halbritt@gmail.com>
2017-10-23 20:15:52 -0700

    break

--- project/main.py
+++ project/main.py
@@ -1,24 +1,25 @@
-import smbus2
-import bme280
 from datetime import datetime
+
+import bme280
+import smbus2
 
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 bme280.load_calibration_params(bus, address)
 
-
 while True:
     try:
         data = bme280.sample(bus, address)
         mydict = {
             "uuid": str(data.id),
-            "timestamp" : datetime.strftime(data.timestamp, "%s%f"),
-            "temperature" : data.temperature,
-            "pressure" : data.pressure,
-            "humidity" : data.humidity
-            }
+            "timestamp": datetime.strftime(data.timestamp, "%s%f"),
+            "temperature": data.temperature,
+            "pressure": data.pressure,
+            "humidity": data.humidity
+        }
 
         print(mydict)
     except:
         print("KABLOOEY!")
+        break
