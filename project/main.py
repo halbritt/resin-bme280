639208fd9289a243705f0e1ce777f02903f20ab3
halbritt <halbritt@gmail.com>
2017-10-23 20:08:30 -0700

    try... except

--- project/main.py
+++ project/main.py
@@ -1,30 +1,24 @@
 import smbus2
 import bme280
+from datetime import datetime
 
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
-
 bme280.load_calibration_params(bus, address)
 
-data = bme280.sample(bus, address)
 
-# the compensated_reading class has the following attributes
-print(data.id)
-print(data.timestamp)
-print(data.temperature)
-print(data.pressure)
-print(data.humidity)
+While True:
+    try:
+        data = bme280.sample(bus, address)
+        mydict = {
+            "uuid": str(data.id),
+            "timestamp" : datetime.strftime(data.timestamp, "%s%f"),
+            "temperature" : data.temperature,
+            "pressure" : data.pressure,
+            "humidity" : data.humidity
+            }
 
-# there is a handy string representation too
-print(data)
-
-mydict = {
-    "uuid": str(data.id),
-    "timestamp" : datetime.strftime(data.timestamp, "%s%f"),
-    "temperature" : data.temperature
-    "pressure" : data.pressure,
-    "humidity" : data.humidity
-    }
-
-print(mydict)
+        print(mydict)
+    except:
+        print("KABLOOEY!")
