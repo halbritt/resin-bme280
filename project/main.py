d3663aa8a4aae31063b3b91676b52982d7afcd4b
halbritt <halbritt@gmail.com>
2017-10-23 17:06:20 -0700

    Revert "WOO"
    This reverts commit 4ae15699ee72a3747e0153254f5bdb425edc0a8a.

--- project/main.py
+++ project/main.py
@@ -1,6 +1,5 @@
 import smbus2
 import bme280
-import csv
 
 port = 1
 address = 0x76
@@ -8,8 +7,8 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus, address)
 
-while true
-
+# the sample method will take a single reading and return a
+# compensated_reading object
 data = bme280.sample(bus, address)
 
 # the compensated_reading class has the following attributes
