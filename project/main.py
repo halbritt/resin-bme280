c70a95b5ea303a36b333898ac51dd590a957d4d7
halbritt <halbritt@gmail.com>
2017-10-23 19:54:21 -0700

    party

--- project/main.py
+++ project/main.py
@@ -1,6 +1,5 @@
 import smbus2
 import bme280
-import csv
 
 port = 1
 address = 0x76
@@ -8,26 +7,24 @@ bus = smbus2.SMBus(port)
 
 bme280.load_calibration_params(bus, address)
 
-while true
+data = bme280.sample(bus, address)
 
-    data = bme280.sample(bus, address)
+# the compensated_reading class has the following attributes
+print(data.id)
+print(data.timestamp)
+print(data.temperature)
+print(data.pressure)
+print(data.humidity)
 
-    # the compensated_reading class has the following attributes
-    print(data.id)
-    print(data.timestamp)
-    print(data.temperature)
-    print(data.pressure)
-    print(data.humidity)
+# there is a handy string representation too
+print(data)
 
-    # there is a handy string representation too
-    print(data)
+mydict = {
+    "uuid": str(data.id),
+    "timestamp" : datetime.strftime(data.timestamp, "%s%f"),
+    "temperature" : data.temperature
+    "pressure" : data.pressure,
+    "humidity" : data.humidity
+    }
 
-    mydict = {
-        "uuid": str(data.id),
-        "timestamp" : datetime.strftime(data.timestamp, "%s%f"),
-        "temperature" : data.temperature
-        "pressure" : data.pressure,
-        "humidity" : data.humidity
-        }
-
-    print(mydict)
+print(mydict)
