d997c9608f62fca3c4d305520e30907e506c6893
halbritt <halbritt@gmail.com>
2017-11-06 21:52:55 -0800

    pie

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 import bme280
+from datetime import datetime
 import smbus2
 from flask import Flask
+from flask import jsonify
 
 port = 1
 address = 0x76
@@ -12,15 +14,17 @@ app = Flask(__name__)
 @app.route('/')
 def get_sample():
     data = bme280.sample(bus, address)
+    fahrenheit = 9.0/5.0 * data.temperature + 32
     mydict = {
         "uuid": str(data.id),
         "timestamp": datetime.strftime(data.timestamp, "%s%f"),
-        "temperature": data.temperature,
+        "temperature": fahrenheit,
         "pressure": data.pressure,
         "humidity": data.humidity
     }
-    return mydict
+    return jsonify(mydict)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
